362fcdd4d507f24450ca819f21bcd1818f8bcae1
Mohamed <mohammednady128@gmail.com>
2023-06-19 16:39:31 +0300

    update validation middleware to handle nested errors

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,4 +1,4 @@
-import {validate} from "class-validator";
+import {ValidationError, validate} from "class-validator";
 import { plainToInstance } from "class-transformer";
 import { RequestHandler } from "express";
 import { ErrorCodes, StatusCodes } from "../utils/errors";
@@ -8,10 +8,32 @@ export function validationMiddleware(type:any,skipMissingProperties=false):Reque
         const errors = await validate(plainToInstance(type, req.body),{skipMissingProperties,whitelist:true});
         if(errors.length>0){
             const errorsRecord:Record<string,any> = {}
-            errors.forEach(err=>{
-                errorsRecord[err.property]=Object.values(err.constraints!).join(",");
-            })
-            return res.status(StatusCodes.BAD_REQUEST).json({...errorsRecord,code:ErrorCodes.VALIDATION})
+            
+            const handleNestedError = (errors: ValidationError[]) => {
+                for (const err of errors) {
+                    if (
+                        Array.isArray(err.children) &&
+                        err.children.length > 0
+                    ) {
+                        handleNestedError(err.children);
+                    }
+                    if (err.constraints) {
+                        errorsRecord[err.property] = Object.values(
+                            err.constraints!
+                        ).join(",");
+                    }
+                }
+            };
+            errors.forEach((err) => {
+                if (!err.constraints) {
+                    handleNestedError(err.children!);
+                } else {
+                    errorsRecord[err.property] = Object.values(
+                        err.constraints!
+                    ).join(",");
+                }
+            });
+            return res.status(StatusCodes.BAD_REQUEST).json({errors:errorsRecord,code:ErrorCodes.VALIDATION})
         }
         next()
     }
