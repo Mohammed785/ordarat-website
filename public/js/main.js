6af34c95d74fd420b84a4e9a116a1a51f62e1784
Mohamed <mohammednady128@gmail.com>
2023-06-26 17:37:59 +0300

    create helper functions

--- public/js/main.js
+++ public/js/main.js
@@ -15,17 +15,30 @@ Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
  * @param {Object} data 
  * @param {Object} headers 
  */
-const submitHandler = (url,method,success,error,data={},headers={})=>{
+const submitHandler = (url,method,success,error,data={},options={})=>{
     $.ajax({
         url,
         method,
         data,
-        headers,
         success,
         error,
+        ...options,
     });
 }
 
+const handleRequestErrors = (errors) => {
+    const errorsJson = errors.responseJSON;
+    console.log(errorsJson)
+    $(".is-invalid").removeClass("is-invalid");
+    if (errorsJson.code === "VALIDATION") {
+        for (const [input, feedback] of Object.entries(errorsJson.errors)) {
+            $(`#${input}`).addClass("is-invalid");
+            $(`#invalid-${input}`).text(feedback);
+        }
+    } else {
+        addAlert("danger", errorsJson.message);
+    }
+};
 const addAlert = (type, message) => {
     const area = $("#alerts-area");
     if (area.children().length>= 3) {
@@ -35,3 +48,72 @@ const addAlert = (type, message) => {
     <span class="alert-text">${message}.</span>
     <button type="button" class="btn-close rtl" data-bs-dismiss="alert" aria-label="Close"></button></div>`);
 };
+const validateInput = (toValidate) => {
+    let valid = true;
+    for (const [input, validation] of Object.entries(toValidate)) {
+        const inputVal = $(`#${input}`).val();
+        let validField = true;
+        $(`#${input}`).removeClass("is-invalid is-valid");
+        for (const [constraint, value] of Object.entries(validation)) {
+            if (
+                (constraint === "required" && inputVal === "") ||
+                value?.notIn?.includes(inputVal)
+            ) {
+                $(`#invalid-${input}`).text(value.message);
+                valid = false;
+                validField = false;
+                continue;
+            }
+            if (constraint === "length") {
+                if (
+                    inputVal.length < value.min ||
+                    inputVal.length > value.max
+                ) {
+                    $(`#invalid-${input}`).text(value.message);
+                    valid = false;
+                    validField = false;
+                    continue;
+                }
+            }
+            if (constraint === "limit") {
+                if (
+                    parseInt(inputVal) < value.min ||
+                    parseInt(inputVal) > value.max
+                ) {
+                    $(`#invalid-${input}`).text(value.message);
+                    valid = false;
+                    validField = false;
+                    continue;
+                }
+            }
+            if (constraint === "regex") {
+                if (!value.regex.test(inputVal)) {
+                    $(`#invalid-${input}`).text(value.message);
+                    valid = false;
+                    validField = false;
+                    continue;
+                }
+            }
+            if(constraint==="isNumber"){
+                if(!parseInt(inputVal)){
+                    $(`#invalid-${input}`).text(value.message);
+                    valid = false;
+                    validField = false;
+                    continue;        
+                }
+            }
+        }
+        if (!validField) {
+            $(`#${input}`).addClass("is-invalid");
+        } else {
+            $(`#${input}`).addClass("is-valid");
+        }
+    }
+    return valid;
+};
+const showToast = (message)=>{
+    const toast = $("#liveToast");
+    toast.children(".toast-body").text(message)
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
+    toastBootstrap.show()
+}
